ba502b7c092799a43d9ae9b52edba17a47fe178f
Bruno <bruno.rsilva2004@gmail.com>
2025-07-16 11:17:49 -0300

    Adição do item rate que calcula a média das avaliações dos filmes cadastrados.

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,27 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
 class MovieSerializer(serializers.ModelSerializer):
+
+    rate = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+        return None
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento deve ser a partir de 1900.')
+        return value
+    
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError('O resumo não pode ter mais de 500 caracteres.')
